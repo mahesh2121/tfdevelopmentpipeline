284b36d4fea5ad57d9dd8b99a3f7193784948bf4
Mahesh Gaikwad <maheshgaikwad128@gmail.com>
2024-07-22 21:23:21 +0530

    Delete modules directory

--- modules/s3/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "this" {
-  count  = var.crete ? 1 : 0
-  bucket = var.name
-
-  force_destroy       = var.force_destroy
-  object_lock_enabled = var.object_lock_enabled
-  tags                = var.tags
-}
